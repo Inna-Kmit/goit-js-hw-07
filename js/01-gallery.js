4bb919c5a24e669609cc4bd5a0a18126ba40cab8
Inna Kmit <inna.km7@gmail.com>
2023-05-24 01:00:29 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+// function galleryList (galleryItems){
+//     return galleryItems.map(({preview, original, description}) => {
+//         return
+//         `<li class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//         <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"/>
+//         </a>
+//         </li>`;
+//     }).join("");
+// }
+const galleryList = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+        </a>
+        </li>`
+).join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryList);
+console.log (galleryList)
+
+gallery.addEventListener ("click", onClickModal);
+
+function onClickModal(evt){
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+		return
+	}
+    // let selectedImage = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+instance.show()
+
+gallery.addEventListener ("keydown", onEscClick);
+
+function onCloseModal() {
+    instance.close()
+    window.removeEventListener ("keydown", onEscClick)
+}
+function onEscClick(evt) {
+    if (evt.key === "Escape") {
+        instance.close()
+    }   
+}
+}
