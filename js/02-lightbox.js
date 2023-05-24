a5b96e48e481ac021efa5f405ab8ce2c3a92da44
Inna Kmit <inna.km7@gmail.com>
2023-05-24 01:52:23 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
+ const gallery = document.querySelector(".gallery")
+
+ const galleryList = galleryItems.map(({preview, original, description})=>
+ `<li class="gallery__item">
+ <a class="gallery__link" href="${original}">
+ <img class="gallery__image" src="${preview}"
+ alt="${description}"/>
+ </a>
+</li>
+ `
+ ).join("");
+
+ const galleryImg = document.querySelector('.gallery__image')
+ gallery.insertAdjacentHTML("beforeend", galleryList)
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
